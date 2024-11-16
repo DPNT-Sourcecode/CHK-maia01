9663d4061155a4794f96d254240023044ea2e5df
sbx_user1051 <sbx_user1051@169.254.118.213>
2024-11-16 17:31:14 +0000

    Sat Nov 16 17:31:14 UTC 2024

--- src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
@@ -4,9 +4,26 @@ namespace BeFaster.App.Solutions.CHK
 {
     public static class CheckoutSolution
     {
+
+        //        Our price table and offers: 
+        //+------+-------+----------------+
+        //| Item | Price | Special offers |
+        //+------+-------+----------------+
+        //| A    | 50    | 3A for 130     |
+        //| B    | 30    | 2B for 45      |
+        //| C    | 20    |                |
+        //| D    | 15    |                |
+        //+------+-------+----------------+
+
+
+        Dictionary<string, int> priceTable = new Dictionary<string, int>();
+
+
+
         public static int ComputePrice(string? skus)
         {
             throw new SolutionNotImplementedException();
         }
     }
 }
+
